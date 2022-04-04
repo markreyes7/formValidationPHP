cf4c3297a5748446f2b8e0f4d8b20cba0158f77f
Mark <mark.reyes7777@gmail.com>
2022-04-04 01:48:39 -0700

    added files for my php project

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'mark');
+    define('DB_PASS', '123456');
+    define('DB_NAME', 'php_dev');
+
+
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if($conn->connect_error){
+        die('Connection Failed' . $conn->connect_error);
+
+    }
+
+  
